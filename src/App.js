60d2f3d36c94c7937da6b4ed717f562e874a888b
Mohamed Mhamdi <mhamdi.mohamed944@gmail.com>
2020-12-08 00:19:31 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import {MyProfilPhoto} from './Component/Profile/ProfilPhoto';
+import {MyFullName} from './Component/Profile/FullName';
+import {MyAddress} from './Component/Profile/Address';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="App-header">
+          <MyProfilPhoto />
+          <MyFullName />
+          <MyAddress />
+        </div>
     </div>
   );
 }
